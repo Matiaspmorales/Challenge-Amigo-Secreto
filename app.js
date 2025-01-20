b8a06c77d816a444af33554f8e36894c1f00f1eb
Matias <matuas248@gmail.com>
2025-01-20 04:01:38 -0300

    Crear función sortearAmigo()

--- app.js
+++ app.js
@@ -4,22 +4,38 @@ let amigos = [];
 function agregarAmigo(){
     if (document.getElementById('amigo').value == ''){
         alert('Por favor, inserte un nombre.');
+        sortearAmigo()
     }else{
         amigos.push(document.getElementById('amigo').value)
         document.getElementById('amigo').value = '';
-        // console.log() para validar que la lista contenga elementos
-        console.log(amigos)
+        sortearAmigo()
         recorrerArray();
     }
 }
 
 
 function recorrerArray(){
-    let listaAmigos = document.getElementById('listaAmigos')
+    resultado.innerHTML = "";
+    let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = "";
     for (amigo of amigos){
         let nuevo_elemento = document.createElement('li');
         nuevo_elemento.textContent = amigo;
         listaAmigos.appendChild(nuevo_elemento);
     }
+}
+
+function sortearAmigo(){
+    if (amigos.length == 0){
+        alert('No hay amigos en la lista para sortear');
+    }
+
+    let indiceAleatorio = Math.floor(Math.random()* amigos.length);
+    let resultado = document.getElementById('resultado');
+    let amigoSorteado = amigos[indiceAleatorio];
+    resultado.innerHTML = "";
+    let nuevo_elemento = document.createElement('li');
+    nuevo_elemento.textContent = amigoSorteado;
+    resultado.appendChild(nuevo_elemento);
+    listaAmigos.innerHTML = "";
 }
